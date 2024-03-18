22b1e266c64cb07c4ac1fa379bfb79adddc74903
ashpatx <thingslearn3@gmail.com>
2024-03-18 17:36:59 -0400

    light dark modes setup

---
+++ lib/pages/theme/light_mode.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  colorScheme: ColorScheme.light(
+    background: Colors.amber.shade50,
+    primary: Colors.amber,
+    secondary: Colors.amber.shade100,
+    tertiary: Colors.white,
+    inversePrimary: Colors.amber.shade900,
+  ),
+);
---
+++ lib/pages/theme/theme_provider.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'dark_mode.dart';
+import 'light_mode.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  //light mode default
+  ThemeData _themeData = lightMode;
+
+  //current theme 
+  ThemeData get themeData => _themeData
+
+  //current theme dark mode 
+  bool get isDarkMode => _themeData == darkMode;
+
+  //set theme
+  set themeData(ThemeData themeData) {
+    _themeData = themeData; 
+    notifyListeners();
+  }
+
+  //toggle theme 
+  voidtoggleTheme() {
+    if (_themeData == lightMode) {
+      themeData = darkMode;
+    } else {
+      themeData = lightMode;
+    }
+  }
+
+}
